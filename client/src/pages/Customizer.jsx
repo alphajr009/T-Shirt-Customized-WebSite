38cf5e64fd29bfd2dd43bb34bb906a1ba282edc6
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-06-17 03:42:00 +0530

    Customizer Screen Start

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
+
+
 
 function Customizer() {
+    const snap = useSnapshot(state);
     return (
-        <div>Customizer</div>
+        <AnimatePresence>
+            {!snap.intro && (
+                <>
+                    <motion.div
+                        key='custom'
+                        className='absolute top-0 left-0 z-10 '
+                        {...slideAnimation('left')}
+                    >
+                        <div className='flex items-center min-h-screen'>
+                            <div className='glassmorphism w-16 border-[2px] rounded-lg flex flex-col justify-center items-center ml-1 py-4 gap-4'>
+                                {EditorTabs.map((tab) => (
+                                    <Tab
+                                        key={tab.name}
+                                        tab={tab}
+                                        handleClick={() => { }}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+
+                    </motion.div>
+                </>
+            )}
+        </AnimatePresence>
     )
 }
 
