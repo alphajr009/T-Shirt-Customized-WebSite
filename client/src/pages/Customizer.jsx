6d8fbe3a2a5a4a3affec01f81fff757111e6d389
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-06-20 02:32:11 +0530

    Error in AI Picker

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -10,31 +10,167 @@ import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
-
-
-function Customizer() {
+const Customizer = () => {
     const snap = useSnapshot(state);
+
+    const [file, setFile] = useState('');
+
+    const [prompt, setPrompt] = useState('');
+    const [generatingImg, setGeneratingImg] = useState(false);
+
+    const [activeEditorTab, setActiveEditorTab] = useState("");
+    const [activeFilterTab, setActiveFilterTab] = useState({
+        logoShirt: true,
+        stylishShirt: false,
+    })
+
+
+    const generateTabContent = () => {
+        switch (activeEditorTab) {
+            case "colorpicker":
+                return <ColorPicker />
+            case "filepicker":
+                return <FilePicker
+                    file={file}
+                    setFile={setFile}
+                    readFile={readFile}
+                />
+            case "aipicker":
+                return <AIPicker
+                    prompt={prompt}
+                    setPrompt={setPrompt}
+                    generatingImg={generatingImg}
+                    handleSubmit={handleSubmit}
+                />
+            default:
+                return null;
+        }
+    }
+
+    const handleSubmit = async (type) => {
+        if (!prompt) return alert("Please enter a prompt");
+
+        try {
+            setGeneratingImg(true);
+
+            const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer $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`,
+                    'OpenAI-Organization': 'org-IYlzz1Kal13Ws8PDRCAU6B4U'
+                },
+                body: JSON.stringify({
+                    prompt
+                })
+            });
+
+
+            console.log('Response:', response);
+            const data = await response.json();
+            console.log('Data:', data);
+
+
+            handleDecals(type, `data:image/png;base64,${data.photo}`)
+        } catch (error) {
+            alert(error)
+        } finally {
+            setGeneratingImg(false);
+            setActiveEditorTab("");
+        }
+    }
+
+    const handleDecals = (type, result) => {
+        const decalType = DecalTypes[type];
+
+        state[decalType.stateProperty] = result;
+
+        if (!activeFilterTab[decalType.filterTab]) {
+            handleActiveFilterTab(decalType.filterTab)
+        }
+    }
+
+    const handleActiveFilterTab = (tabName) => {
+        switch (tabName) {
+            case "logoShirt":
+                state.isLogoTexture = !activeFilterTab[tabName];
+                break;
+            case "stylishShirt":
+                state.isFullTexture = !activeFilterTab[tabName];
+                break;
+            default:
+                state.isLogoTexture = true;
+                state.isFullTexture = false;
+                break;
+        }
+
+
+
+        setActiveFilterTab((prevState) => {
+            return {
+                ...prevState,
+                [tabName]: !prevState[tabName]
+            }
+        })
+    }
+
+    const readFile = (type) => {
+        reader(file)
+            .then((result) => {
+                handleDecals(type, result);
+                setActiveEditorTab("");
+            })
+    }
+
     return (
         <AnimatePresence>
             {!snap.intro && (
                 <>
                     <motion.div
-                        key='custom'
-                        className='absolute top-0 left-0 z-10 '
+                        key="custom"
+                        className="absolute top-0 left-0 z-10"
                         {...slideAnimation('left')}
                     >
-                        <div className='flex items-center min-h-screen'>
-                            <div className='glassmorphism w-16 border-[2px] rounded-lg flex flex-col justify-center items-center ml-1 py-4 gap-4'>
+                        <div className="flex items-center min-h-screen">
+                            <div className="editortabs-container tabs">
                                 {EditorTabs.map((tab) => (
                                     <Tab
                                         key={tab.name}
                                         tab={tab}
-                                        handleClick={() => { }}
+                                        handleClick={() => setActiveEditorTab(tab.name)}
                                     />
                                 ))}
+
+                                {generateTabContent()}
                             </div>
                         </div>
+                    </motion.div>
 
+                    <motion.div
+                        className="absolute z-10 top-5 right-5"
+                        {...fadeAnimation}
+                    >
+                        <CustomButton
+                            type="filled"
+                            title="Go Back"
+                            handleClick={() => state.intro = true}
+                            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                        />
+                    </motion.div>
+
+                    <motion.div
+                        className='absolute z-10 bottom-5 right-0 left-0 w-full flex justify-center items-center flex-wrap gap-4'
+                        {...slideAnimation("up")}
+                    >
+                        {FilterTabs.map((tab) => (
+                            <Tab
+                                key={tab.name}
+                                tab={tab}
+                                isFilterTab
+                                isActiveTab={activeFilterTab[tab.name]}
+                                handleClick={() => handleActiveFilterTab(tab.name)}
+                            />
+                        ))}
                     </motion.div>
                 </>
             )}
