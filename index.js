7397e6c490776d815bd584a67d9728c5bacc642f
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-06-20 16:05:55 +0530

    Update Index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
 
@@ -25,4 +26,4 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
-app.listen(8080, () => console.log('Server has started on port 8080'));
+app.listen(port, () => console.log('Server has started on port 8080'));
