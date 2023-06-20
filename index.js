4280f26fb96f144c788bed15c7c038746f0f9aa0
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-06-20 16:11:50 +0530

    Update index

--- index.js
+++ index.js
@@ -8,22 +8,13 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-
+app.use(express.json({ limit: "50mb" }))
 const port = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-
-    const apiKey = process.env.OPENAI_API_KEY;
-
-
-    res.render('index', { apiKey });
-});
 
 app.use("/api/v1/dalle", dalleRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Hello from DALL.E" })
+})
 
-app.listen(port, () => console.log('Server has started on port 8080'));
+app.listen(port, () => console.log('Server has started on port 8080'))
