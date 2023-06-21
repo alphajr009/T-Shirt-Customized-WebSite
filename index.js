0c3db8a89fb49faf25b25312c19946282357579f
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-06-21 15:06:23 +0530

    u

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 
     // Serve the index.html file for all non-API routes
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
     });
 }
 
